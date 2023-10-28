35df853caf60d4b880fdfbec6c00acb6a5879d01
danierazome <j.erao@uniandes.edu.co>
2023-10-27 21:20:39 -0500

    feature: Integracion de todas las capas fue agregada

--- app/src/main/java/com/example/vinilos/ui/screen/HomeVisitantScreen.kt
+++ app/src/main/java/com/example/vinilos/ui/screen/HomeVisitantScreen.kt
@@ -4,8 +4,39 @@ package com.example.vinilos.ui.screen
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.vinilos.model.Album
+import com.example.vinilos.ui.viewmodel.AlbumUIState
+import com.example.vinilos.ui.viewmodel.AlbumViewModel
 
 @Composable
 fun HomeVisitant( modifier: Modifier = Modifier) {
-    Text(text = "Home")
+    val albumViewModel: AlbumViewModel =
+        viewModel(factory = AlbumViewModel.Factory)
+    
+    AlbumCarrousel(albumsUiState = albumViewModel.albumsUiState)
+}
+
+@Composable
+fun AlbumCarrousel( albumsUiState: AlbumUIState, modifier: Modifier = Modifier) {
+    when (albumsUiState) {
+        is AlbumUIState.Loading -> Loading()
+        is AlbumUIState.Success -> AlbumsOk(albums = albumsUiState.albums)
+        is AlbumUIState.Error -> ErrorAlbum()
+    }
+}
+
+@Composable
+fun Loading( modifier: Modifier = Modifier) {
+    Text(text = "Loading")
+}
+
+@Composable
+fun ErrorAlbum( modifier: Modifier = Modifier) {
+    Text(text = "ErrorAlbum")
+}
+
+@Composable
+fun AlbumsOk(albums: List<Album>, modifier: Modifier = Modifier) {
+    Text(text = albums.toString())
 }
