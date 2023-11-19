12782e9662c4b2bce335a42d1638bd9101144281
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2023-11-19 17:25:46 -0500

    Vista de Collectors

--- app/src/main/java/com/example/vinilos/model/Collector.kt
+++ app/src/main/java/com/example/vinilos/model/Collector.kt
@@ -3,4 +3,7 @@ package com.example.vinilos.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Collector (val name: String)
+data class Collector (val name: String, val comments: List<Comment>?)
+
+@Serializable
+class Comment(val id: Int, val description: String, val rating: Int)
